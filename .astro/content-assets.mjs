cc4510608a90ac51b2d4a3a8136e02a956a41f26
tshepiso <tshepi.tech@gmail.com>
2025-01-21 14:09:37 +0100

    image fix

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,4 @@
 
-import __ASTRO_IMAGE_IMPORT_t8jPX from "../public/home-blog-card.png?astroContentImageFlag=&importer=posts%2Fmy-first-post.md";
 import __ASTRO_IMAGE_IMPORT_Z13I0XN from "../public/logo.png?astroContentImageFlag=&importer=posts%2Fmy-first-post.md";
-export default new Map([["../public/home-blog-card.png?astroContentImageFlag=&importer=posts%2Fmy-first-post.md", __ASTRO_IMAGE_IMPORT_t8jPX], ["../public/logo.png?astroContentImageFlag=&importer=posts%2Fmy-first-post.md", __ASTRO_IMAGE_IMPORT_Z13I0XN]]);
+export default new Map([["../public/logo.png?astroContentImageFlag=&importer=posts%2Fmy-first-post.md", __ASTRO_IMAGE_IMPORT_Z13I0XN]]);
 		
